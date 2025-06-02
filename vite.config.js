d87898818062960dba6868597603bebf2c5f4181
pranay0217 <shitpranay@gmail.com>
2025-06-02 12:33:41 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/', // good for deployment at root domain
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // optional: default is 'dist'
+  },
+  server: {
+    // during development, support SPA fallback
+    historyApiFallback: true,
+  },
+});
